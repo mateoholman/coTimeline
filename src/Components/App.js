f4ca4258cea4319ee40368080cf238184cc5fb83
Matthew Holman <mateoholman@gmail.com>
2017-05-23 14:39:36 -0500

    Changed vanilla folder structure

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../CSS/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello There!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
